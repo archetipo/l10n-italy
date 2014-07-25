c6486fb27808d42f91e15e84542bc3b679d738d2
Davide Corio <davide.corio@lsweb.it>
2014-07-25 20:51:28 +0200

    [ADD] l10n_it_opendata

---
+++ l10n_it_opendata/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Davide Corio <davide.corio@lsweb.it>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Italian Localization - OpenData',
+    'version': '0.1',
+    'category': 'Localization/Italy',
+    'summary': 'OpenData base module',
+    'description': """
+Base module providing common features for OpenData.
+    """,
+    'author': 'OpenERP Italian Community',
+    'website': 'http://www.openerp-italia.org',
+    'license': 'AGPL-3',
+    "depends": [
+        'base_setup'],
+    "data": [
+        'views/config_view.xml',
+    ],
+    "test": [],
+    "demo": [],
+    "installable": True
+}
