0de22b0672cf56e2b31fe17753634ac080565257
Davide Corio <davide.corio@lsweb.it>
2014-07-25 19:00:54 +0200

    [ADD] l10n_it_indicepa

---
+++ l10n_it_indicepa/models/company.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Davide Corio <davide.corio@lsweb.it>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class res_company(orm.Model):
+    _inherit = 'res.company'
+    _columns = {
+
+    }
+
+
+class account_config_settings(orm.TransientModel):
+    _inherit = 'account.config.settings'
+    _columns = {
+
+    }
+
+    def onchange_company_id(self, cr, uid, ids, company_id, context=None):
+        res = super(account_config_settings, self).onchange_company_id(
+            cr, uid, ids, company_id, context=context)
+        if company_id:
+            company = self.pool.get('res.company').browse(
+                cr, uid, company_id, context=context)
+            res['value'].update({})
+        else:
+            res['value'].update({})
+        return res
